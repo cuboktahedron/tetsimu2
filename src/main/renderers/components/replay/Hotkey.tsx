24145e83c2208e54c06a68c371fe77e02ed251b3
cuboktahedron <cuboktahedron@gmail.com>
2020-12-16 00:07:14 +0900

    Add hotkeys in replay mode

--- src/main/renderers/components/replay/Hotkey.tsx
+++ src/main/renderers/components/replay/Hotkey.tsx
@@ -1,6 +1,97 @@
+import { backwardStep, forwardStep } from "ducks/replay/actions";
+import { getReplayConductor } from "ducks/replay/selectors";
 import React from "react";
+import { OperationKey } from "utils/tetsimu/operationKey";
+import { ReplayContext } from "./Replay";
 
 const HotKey: React.FC = () => {
+  const { state, dispatch } = React.useContext(ReplayContext);
+
+  const initialKeys = {
+    ArrowLeft: new OperationKey({ interval1: 200, interval2: 100 }),
+    ArrowRight: new OperationKey({ interval1: 200, interval2: 100 }),
+  };
+
+  const [subKeys, setSubKeys] = React.useState(initialKeys);
+
+  React.useEffect(() => {
+    const callback = (e: KeyboardEvent) => {
+      const target = e.target as Element;
+      const nodeName = target?.nodeName.toLowerCase();
+      if (nodeName === "input") {
+        return;
+      }
+
+      if (e.shiftKey || e.altKey || e.ctrlKey) {
+        return;
+      }
+
+      switch (e.key.toLowerCase()) {
+        case "arrowleft":
+          subKeys["ArrowLeft"].down();
+          break;
+        case "arrowright":
+          subKeys["ArrowRight"].down();
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+
+    return () => {
+      document.removeEventListener("keydown", callback);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    const callback = (e: KeyboardEvent) => {
+      const target = e.target as Element;
+      const nodeName = target?.nodeName.toLowerCase();
+      if (nodeName === "input") {
+        return;
+      }
+
+      switch (e.key.toLowerCase()) {
+        case "arrowleft":
+          subKeys["ArrowLeft"].up();
+          break;
+        case "arrowright":
+          subKeys["ArrowRight"].up();
+          break;
+      }
+    };
+
+    document.addEventListener("keyup", callback);
+
+    return () => {
+      document.removeEventListener("keyup", callback);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    const timerId = setInterval(() => {
+      for (const key of Object.values(subKeys)) {
+        key.refresh();
+      }
+
+      setSubKeys({ ...subKeys });
+    });
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (subKeys["ArrowLeft"].active) {
+      dispatch(backwardStep(getReplayConductor(state)));
+    }
+
+    if (subKeys["ArrowRight"].active) {
+      dispatch(forwardStep(getReplayConductor(state)));
+    }
+  }, [subKeys]);
+
   return <div />;
 };
 
