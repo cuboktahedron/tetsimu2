f3fed41aeec058ea16205d3eb416b46bcaf74423
cuboktahedron <cuboktahedron@gmail.com>
2020-12-29 10:10:07 +0900

    Implement deserialization each element

---
+++ src/main/utils/tetsimu/deserializer.ts
@@ -0,0 +1,143 @@
+import {
+  Direction,
+  FieldCellValue,
+  FieldState,
+  HoldState,
+  MAX_FIELD_HEIGHT,
+  ReplayStep,
+  ReplayStepType,
+  SpinType,
+  Tetromino,
+} from "types/core";
+import BitReader from "utils/bitReader";
+import RadixConverter from "utils/radixConverter";
+import { StepType } from "./serializer";
+
+export class DeserializeError extends Error {}
+
+const radixConverter = new RadixConverter(64);
+
+export const deserializeField = (dataString: string): FieldState => {
+  const data = base64ToBytesbytes(dataString);
+  const reader = new BitReader(data);
+  const fieldCellValues: FieldCellValue[] = [];
+
+  while (!reader.eof) {
+    const value = reader.read(4);
+    if (value <= FieldCellValue.GARBAGE) {
+      fieldCellValues.push(value as FieldCellValue);
+    } else {
+      break;
+    }
+  }
+
+  const lackOfCellNums = MAX_FIELD_HEIGHT * 10 - fieldCellValues.length;
+  const allFieldCellValues = new Array(lackOfCellNums)
+    .fill(FieldCellValue.NONE)
+    .concat(fieldCellValues);
+
+  const field: FieldState = [...new Array(MAX_FIELD_HEIGHT)]
+    .map((_, i) => allFieldCellValues.slice(i * 10, (i + 1) * 10))
+    .reverse();
+
+  return field;
+};
+
+export const deserializeNexts = (dataString: string): Tetromino[] => {
+  const data = base64ToBytesbytes(dataString);
+  const reader = new BitReader(data);
+  const nexts: Tetromino[] = [];
+
+  while (!reader.eof) {
+    const value = reader.read(3);
+    if (value !== Tetromino.NONE) {
+      nexts.push(value as Tetromino);
+    } else {
+      break;
+    }
+  }
+
+  return nexts;
+};
+
+export const deserializeHold = (dataString: string): HoldState => {
+  const value = radixConverter.convertToDecimal(dataString);
+  const type = ((value & 0b1110) >> 1) as Tetromino;
+  const canHold = (value & 1) === 0;
+
+  return {
+    type,
+    canHold,
+  };
+};
+
+export const deserializeSteps = (dataString: string): ReplayStep[] => {
+  const data = base64ToBytesbytes(dataString);
+  const reader = new BitReader(data);
+  const steps: ReplayStep[] = [];
+
+  while (!reader.eof) {
+    const stepType = reader.read(4) as StepType;
+
+    if (stepType === StepType.EndOfStep) {
+      break;
+    }
+
+    switch (stepType) {
+      case StepType.Drop:
+        deserializeDropStep(reader, steps);
+        break;
+      case StepType.DropWithAttacked:
+        throw new DeserializeError("Not supported yet");
+      case StepType.Hold:
+        deserializeHoldStep(steps);
+        break;
+    }
+  }
+
+  return steps;
+};
+
+const deserializeHoldStep = (steps: ReplayStep[]) => {
+  steps.push({
+    type: ReplayStepType.Hold,
+  });
+};
+
+const deserializeDropStep = (reader: BitReader, steps: ReplayStep[]) => {
+  const posValue = reader.read(8);
+  const x = posValue % 10;
+  const y = Math.floor(posValue / 10);
+  const dir = reader.read(2) as Direction;
+  const spinType = reader.read(2) as SpinType;
+
+  steps.push({
+    type: ReplayStepType.Drop,
+    dir,
+    pos: {
+      x,
+      y,
+    },
+    spinType,
+  });
+
+  steps.push({
+    type: ReplayStepType.HardDrop,
+  });
+};
+
+const base64ToBytesbytes = (data: string): Uint8Array => {
+  const base64Data = data
+    .replace(/\./g, "+")
+    .replace(/\-/g, "/")
+    .replace(/_/g, "=");
+
+  const bs = atob(base64Data);
+  const len = bs.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    bytes[i] = bs.charCodeAt(i);
+  }
+
+  return bytes;
+};
---
+++ src/test/utils/tetsimu/deserializer.spec.ts
@@ -0,0 +1,84 @@
+import { Direction, SpinType, Tetromino } from "types/core";
+import {
+  deserializeField,
+  deserializeHold,
+  deserializeNexts,
+  deserializeSteps,
+} from "../../../main/utils/tetsimu/deserializer";
+import { makeField } from "./testUtils/makeField";
+import { makeHold } from "./testUtils/makeHold";
+import {
+  makeReplayDropStep,
+  makeReplayHardDropStep,
+  makeReplayHoldStep,
+} from "./testUtils/makeReplayStep";
+import { makeTetrominos } from "./testUtils/makeTetrominos";
+
+describe("deserializeField", () => {
+  it("should deserialize field", function () {
+    const actual = deserializeField("EjRWeBI0VngA");
+
+    const expected =
+      // prettier-ignore
+      makeField(
+        "NNIJLOSTZG",
+        "IJLOSTZGNN"
+      );
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("deserializeHold", () => {
+  it("should deserialize hold with none", function () {
+    const actual = deserializeHold("0");
+    const expected = makeHold(Tetromino.NONE, true);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should deserialize hold with exchangeable 'I'", function () {
+    const actual = deserializeHold("2");
+    const expected = makeHold(Tetromino.I, true);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should deserialize hold with not exchangeable 'I'", function () {
+    const actual = deserializeHold("3");
+    const expected = makeHold(Tetromino.I, false);
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("deserializeNexts", () => {
+  it("should deserialize nexts", function () {
+    const actual = deserializeNexts("Kcu4");
+    const expected = makeTetrominos("IJLOSTZ");
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should deserialize nexts with padding", function () {
+    const actual = deserializeNexts("KAA_");
+    const expected = makeTetrominos("IJ");
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("deserializeSteps", () => {
+  it("should deserialize steps", function () {
+    const actual = deserializeSteps("AAAQ0G8_");
+    const expected = [
+      makeReplayDropStep(Direction.UP, 0, 0),
+      makeReplayHardDropStep(),
+      makeReplayHoldStep(),
+      makeReplayDropStep(Direction.LEFT, 8, 20, SpinType.Spin),
+      makeReplayHardDropStep(),
+    ];
+
+    expect(actual).toEqual(expected);
+  });
+});
